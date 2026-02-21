2317d61abbcd4ad7c896e44e7729a1664cea1163
shohanean <hossain_shohan1@live.com>
2026-02-21 23:21:13 +0600

    init frontend design setup

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    function index(){
-        return view('index');
+    function index()
+    {
+        return view('frontend.index');
     }
 }
